36a6159f5eee7e180e1817683a9c2b10cd689df2
Ulugbek <79435067+Chelovek2343@users.noreply.github.com>
2024-09-20 12:28:31 +0600

    Update main.py
    Clear terminal and give only the result

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from os import system
+
 a = round(int(input('Write the first number: ')))
 b = round(int(input('Write the second number: ')))
 operators = input('Write the operations u want(+ - * /:) ')
 if(operators == '+'):
+    _ = system('cls')
     print(a + b)
 elif(operators == '-'):
+    _ = system('cls')
     print(a - b)
 elif(operators == '*'):
+    _ = system('cls')
     print(a * b)
 else:
+    _ = system('cls')
     print(a / b)
